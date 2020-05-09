230a4efc2d92e15346894c4915f0cb43417850f2
浅倉麗子 <asakurareiko@protonmail.ch>
2020-05-08 20:50:02 -0400

    Apply copyright notice to all files

--- config.c
+++ config.c
@@ -1,6 +1,6 @@
 /*
-Sharpscale
-Copyright (C) 2020 浅倉麗子
+This file is part of Sharpscale
+Copyright 2020 浅倉麗子
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
