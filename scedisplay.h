bff16b6d182d63542c7dec7181e9ca1cde433675
浅倉麗子 <asakurareiko@protonmail.ch>
2020-08-08 21:12:26 -0400

    Apply copyright symbol ©

--- scedisplay.h
+++ scedisplay.h
@@ -1,6 +1,6 @@
 /*
 This file is part of Sharpscale
-Copyright 2020 浅倉麗子
+Copyright © 2020 浅倉麗子
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
